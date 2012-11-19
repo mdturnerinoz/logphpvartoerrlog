1d796c34da78bebf4cb6f8af0d914d7d2bd3dfeb
Marty Turner <developer@mdturner.com>
2012-11-20 10:32:39 +1100

    initial add to the repos

---
+++ README.md
@@ -0,0 +1,55 @@
+********************************************************************
+
+ logVarToErrorLog - print/log php variable information to the system
+ or user PHP error log.
+
+ ********************************************************************
+
+ logVarToErrorLog: log a variable to a PHP error log for debugging.
+
+ If you are using the system error log, then set $logFile to null; else, 
+ set it to the location where you want the logging to go.  Note that if you 
+ do want to use the system error log, then you can use the settings noted 
+ below to minimize output to deal with.  See the php.net doc if you decide 
+ to change these settings; else, using your own log file will be more 
+ direct and simpler to manage (and not system-wide).  
+
+ The php.ini file you use should be located near the executable
+ directory where the php bin lives; search for the following variables
+ as you see fit:
+
+ error_reporting = E_ALL & ~E_NOTICE & ~E_WARNING 
+ display_errors = On
+ log_errors = On
+ error_log = /path/to/phperror.txt
+
+ The code in logVarToErrorLog.php consists of multiple parts: the first is 
+ the actual function itself () and the second is some code to test it out.  
+
+ You will need to follow the following steps to test the code or place it 
+ into a tested php application environment: 
+
+ 1. I tested the code with Active State Komodo IDE 7.1. It has its own
+    test environment which did not need the code to run under a web
+    server (e.g. Apache).
+
+ 2. Place the php code in a webserver path (e.g. Apache htdocs) so it
+    can be executed by calls to it. Note that this will require global
+    inclusion into the testing php code in some fashion.
+
+ 3. Copy/cut and paste the logVarToErrorLog into an appropriate location
+    within the php code that you need to test/display variables with this
+    the logVarToErrorLog function.
+
+ Also note that you will need to either directly copy the php.ini flags I have
+ used above, or copy and vary them as you see fit based on the description of
+ each variable (presently denoted within any current php.ini files).
+
+ This code commas with absolution no warranty whatsoever, so use it at your own
+ peril :0) .
+
+ Marty Turner
+ November 2012
+ mdturnerinoz@gmail.com (I seldom read this email, but can try... ;0) )
+
+
---
+++ logVarToErrorLog.php
@@ -0,0 +1,76 @@
+<?php
+//
+// logVarToErrorLog: log a variable to a PHP error log for debugging.
+//
+// Begin code to place in your code:
+//
+// If you are using the system error log, then set $logFile to null; else,
+// set it to the location where you want the logging to go. Note that
+// if you do want to use the system error log, then you can use the settings
+// noted below to minimize output to deal with. See the php.net doc if you
+// decide to change these settings; else, using your own log file will be
+// more direct and simpler to manage (and not system-wide).
+//
+// The php.ini file you use should be located near the executable
+// directory where the php bin lives; search for the following variables
+// as you see fit:
+//
+// error_reporting = E_ALL & ~E_NOTICE & ~E_WARNING 
+// display_errors = On
+// log_errors = On
+// error_log = /path/to/phperror.txt
+//
+function logVarToErrorLog( $varToLog, $logFile )
+{
+   // Timestamps only done for the system log, so we have to do our own
+   // if a logfile name has been passed to us. We do it using the same
+   // format that the system log would provide (continuity, ya know).
+   $myDate = date("[j-M-Y G:H:s]");
+   
+   if ($logFile == null)
+      error_log("*****  Begin logVarToErrorLog  *****");
+   else
+   {
+      error_log("$myDate *****  Begin logVarToErrorLog  *****\n", 3, $logFile);
+   }
+   ob_start();
+   var_dump($varToLog);
+   print_r($varToLog);
+   $buffer = ob_get_contents();
+   ob_end_clean();
+   
+   if ($logFile == null)
+   {
+      error_log($buffer);
+      error_log("*****  End   logVarToErrorLog  *****");
+   }
+   else
+   {
+      error_log($buffer, 3, $logFile);
+      error_log("\n$myDate *****  End   logVarToErrorLog  *****\n", 3, $logFile); 
+   }
+} 
+// end code to place in your code.
+//
+// The code from here on is just used to test the logVarToErrorLog function.
+// Remove it before integrating logVarToErrorLog into your code.
+//
+// Uncomment the next line to use a specific log file and comment out the
+// one after it (after placing your log file name in of course  :0) ).
+// $logFile = "<os-specific-path-to-logfile>php-errors.log";
+$logFile = null;  // null causes the system error log to be used (see php.ini)
+
+$array = array("foo" => "bar",
+               "my" => "thing",
+               1 => 2,
+               "hallo" => "world");
+
+logVarToErrorLog( $array, $logFile );
+logVarToErrorLog(" some stuff ", $logFile);
+
+$myDate = date("[j-M-Y G:H:s]" . "\n");
+print_r($myDate);
+
+die ("We are done...\n"); 
+
+?>
